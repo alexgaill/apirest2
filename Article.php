2ba13f613b0490446abaa11503e349f9de445311
alexandre gaillot <alexandre@MacBook-Air-de-alexandre.local>
2020-10-07 14:25:39 +0200

    fin organisation du code

--- Article.php
+++ Article.php
@@ -1,70 +1,8 @@
 <?php
 
-require "Database.php";
+require "General.php";
 
-class Article {
+class Article extends General{
     
-    public function __construct()
-    {
-        $this->db = new Database();
-    }
-    /**
-     * Get all informations about article
-     *
-     */
-    public function getAll()
-    {
-        $this->db->query("SELECT * FROM article");
-    }
-
-    /**
-     * Get one line of article
-     *
-     * @param int $id
-     */
-    public function getOne($id)
-    {
-        $this->db->query("SELECT * FROM article WHERE id=$id", true);
-    }
-
-    /**
-     * Save informations in article
-     *
-     * @param array $param
-     */
-    public function save($param)
-    {
-        $statement = "INSERT INTO article (title, content, categorie_id) VALUES (:title, :content, :categorie_id)";
-        $this->db->prepare($statement, "save", $param);
-    }
-
-    /**
-     * delete one line in article
-     *
-     * @param int $id
-     */
-    public function delete($id)
-    {
-        $statement = "DELETE FROM article WHERE id=$id";
-        $this->db->prepare($statement, "delete");
-    }
-
-    /**
-     * Modify data in article
-     *
-     * @param int $id
-     * @param string $param
-     */
-    public function put($id, $param){
-        $put = explode(",", $param);
-        $data= array();
-
-        foreach ($put as $value) {
-            $elmt = explode(":", $value);
-            $data[$elmt[0]] = $elmt[1];
-        }
-
-        $statement = "UPDATE article SET title= :title, content= :content, categorie_id= :categorie_id WHERE id=$id";
-        $this->db->prepare($statement, "put", $data);
-    }
+   protected $table = "article";
 }
---
+++ General.php
@@ -0,0 +1,89 @@
+<?php
+
+require "Database.php";
+
+class General {
+
+    public $db;
+    protected $table;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+        // $this->table = strtolower($this->table);
+    }
+    /**
+     * Get all informations
+     *
+     */
+    public function getAll()
+    {
+        $this->db->query("SELECT * FROM $this->table");
+    }
+
+    /**
+     * Get one line of information
+     *
+     * @param int $id
+     */
+    public function getOne($id)
+    {
+        $this->db->query("SELECT * FROM $this->table WHERE id=$id", true);
+    }
+
+    /**
+     * Save informations in Db
+     *
+     * @param array $param
+     */
+    public function save($param)
+    {
+        $statement = "INSERT INTO $this->table (";
+        $values = " VALUES (";
+        foreach ($param as $key => $value) {
+            $statement .= $key. ", ";
+            $values .= ":".$key. ", ";
+        }
+        $statement = substr($statement, 0, -2) . ") ";
+        $values = substr($values, 0, -2) . ")";
+
+        $statement .= $values;
+        var_dump($statement);
+        // $this->db->prepare($statement, "save", $param);
+    }
+
+    /**
+     * delete one line in categorie
+     *
+     * @param int $id
+     */
+    public function delete($id)
+    {
+        $statement = "DELETE FROM $this->table WHERE id=$id";
+        $this->db->prepare($statement, "delete");
+    }
+
+    /**
+     * Modify data in categorie
+     *
+     * @param int $id
+     * @param string $param
+     */
+    public function put($id, $param){
+        $put = explode(",", $param);
+        $data= array();
+
+        foreach ($put as $value) {
+            $elmt = explode(":", $value);
+            $data[$elmt[0]] = $elmt[1];
+        }
+
+        $statement = "UPDATE $this->table SET ";
+        foreach ($data as $key => $value) {
+            $statement .= $key."=".$value.", ";
+        }
+        $statement = substr($statement, 0,-2);
+        $statement .= " WHERE id=$id";
+        $this->db->prepare($statement, "put", $data);
+    }
+}
